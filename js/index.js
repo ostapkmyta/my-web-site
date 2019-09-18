be5b698d5e2b32cd1fce1f53f6da8f0b58642db8
Ostap Kmyta <ostapikkmyta@gmail.com>
2019-09-18 15:28:17 +0000

    formated

--- js/index.js
+++ js/index.js
@@ -4,8 +4,3 @@ const inst = document.querySelector('.logos .inst');
 //clock
 const clock = document.querySelector('.clock');
     setInterval( () => clock.innerText = (new Date()).toLocaleTimeString(), 1000 );
-
-
-
-
-
